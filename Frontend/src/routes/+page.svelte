eb652c94f8d1b17fbbffddbd62b95f13a2429b45
Dean <deang467@gmail.com>
2024-03-19 11:36:20 -0400

    express connection

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+	// Acting as an API.
+	// Connection between front and back including the database
+	let express_URL = 'http://localhost:3000/';
+	function helloWorld() {
+		fetch(express_URL);
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
